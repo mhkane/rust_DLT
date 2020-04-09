8ffee938206935e6097fd320bdf8cc9b69f7aa54
mhkane <hassanmohamed0407@gmail.com>
2020-04-09 18:58:48 -0400

    Added command line interface to Blockchain

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use blockchainlib::*;
+use std::io;
 
 fn main () {
 	let difficulty = 0x000fffffffffffffffffffffffffffff;
@@ -24,14 +25,57 @@ fn main () {
 
     println!("Mined genesis block {:?}", &genesis_block);
 
-    let mut last_hash = genesis_block.hash.clone();
+    let last_hash = genesis_block.hash.clone();
 
     let mut blockchain = Blockchain::new();
 
 
     blockchain.update_with_block(genesis_block).expect("Failed to add gennesis block");
 
-    let mut second_block = Block::new(1,now(),vec![0;32], vec![
+    println!("Here are the blockchain blocks {:?}", &blockchain.blocks);
+
+    let mut sender = String::new();
+
+    let mut sendee = String::new();
+
+    println!("Hello, which account are you?");
+
+    match io::stdin().read_line(&mut sender) {
+        Ok(_) => {
+            println!("Success, you are: {}", sender);
+        },
+        Err(e) => println!("Oops! Something went wrong: {}", e)
+    }
+
+    println!("Hello {}, who do you want to send that money to? ", sender);
+
+    match io::stdin().read_line(&mut sendee) {
+        Ok(_) => {
+            println!("Success, you want to send money to: {}", sendee);
+        },
+        Err(e) => println!("Oops! Something went wrong: {}", e)
+    }
+
+
+    let mut amount_string = String::new();
+
+    let mut amount = 0;
+
+    println!("How many coins do you want to send?");
+
+    match io::stdin().read_line(&mut amount_string) {
+        Ok(_) => {
+             amount = amount_string
+        .trim()
+        .parse()
+        .expect("Wanted a number");
+        },
+        Err(e) => println!("Oops! Something went wrong: {}", e)
+    }
+
+    println!("This is how much {}", amount);
+
+    let mut second_block = Block::new(1,now(),last_hash, vec![
         Transaction {
             inputs: vec![ ],
             outputs: vec![
@@ -44,7 +88,26 @@ fn main () {
                     value: 7,
                 },
             ],
-        }
+        }, 
+        Transaction {
+            inputs: vec![
+                transaction::Output {
+                    to_addr: "Alice".to_owned(),
+                    value: 50,
+                },
+                transaction::Output {
+                    to_addr: "Bob".to_owned(),
+                    value: 7,
+                },
+            ],
+
+            outputs: vec![
+                transaction::Output {
+                    to_addr: sendee.to_owned(),
+                    value: amount,
+                },
+            ],
+        } 
         ], difficulty);
 
 
@@ -52,9 +115,9 @@ fn main () {
 
     println!("Mined second block {:?}", &second_block);
 
-
     blockchain.update_with_block(second_block).expect("Failed to add Second block");
 
+    println!("Here are the blockchain blocks {:?}", &blockchain.blocks[1].transactions[1].outputs);
 
 
 }
